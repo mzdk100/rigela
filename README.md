74d086167dabc13bb983b1c75d0cb82a0af2f8a5
Administrator <mzdk100@foxmail.com>
2023-12-10 13:57:02 +0800

    首次提交。

---
+++ README.md
@@ -0,0 +1,3 @@
+[Rust 程序设计语言 简体中文版](https://kaisery.github.io/trpl-zh-cn)
+[Rust for Windows 仓库](https://github.com/microsoft/windows-rs)
+[Rust for Windows 文档](https://microsoft.github.io/windows-docs-rs/)
---
+++ main/src/main.rs
@@ -0,0 +1,30 @@
+#![windows_subsystem = "windows"]
+
+use windows::core::{h, Result};
+use windows::Media::{
+    Core::MediaSource,
+    Playback::{
+        MediaPlaybackItem,
+        MediaPlayer
+    },
+    SpeechSynthesis::SpeechSynthesizer
+};
+#[link(name = "peeper.dll", kind = "static")]
+extern "C" {
+    fn add(left: usize, right: usize) -> usize;
+}
+fn main() -> Result<()> {
+    unsafe { println!("Hello, {}!", add(3, 4)); }
+    futures::executor::block_on(main_async())
+}
+async fn main_async() -> Result<()> {
+    let player = MediaPlayer::new()?;
+    let synth = SpeechSynthesizer::new()?;
+    let stream = synth.SynthesizeTextToStreamAsync(h!("你好"))?.await?;
+    stream.GetInputStreamAt(0)?;
+    let source = MediaSource::CreateFromStream(&stream, &stream.ContentType()?)?;
+    let item = MediaPlaybackItem::Create(&source)?;
+    player.SetSource(&item)?;
+    player.Play()?;
+    Ok(())
+}
