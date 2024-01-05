b095a6eb97f48fe599893928ccfac0864bbebad3
Liwen <Lwboy@Live.com>
2024-01-05 17:47:14 +0800

    添加brawseable, 有bug

--- main/src/context.rs
+++ main/src/context.rs
@@ -11,13 +11,20 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-use std::sync::Arc;
-use tokio::runtime::Handle;
-use win_wrap::uia::{UiAutomation, UiAutomationElement};
 use crate::commander::Commander;
 use crate::performer::{Performer, Speakable};
 use crate::resources::ResourceAccessor;
 use crate::terminator::Terminator;
+use std::sync::Arc;
+use tokio::runtime::Handle;
+use win_wrap::browser::{Browseable, FormBrowser};
+use win_wrap::uia::{UiAutomation, UiAutomationElement};
+
+impl Speakable for dyn Browseable {
+    fn get_sentence(&self) -> String {
+        format!("{}: {}", self.get_name(), self.get_role())
+    }
+}
 
 impl Speakable for UiAutomationElement {
     fn get_sentence(&self) -> String {
@@ -31,17 +38,19 @@ pub struct Context {
     pub(crate) resource_accessor: Arc<ResourceAccessor>,
     pub(crate) performer: Arc<Performer>,
     pub(crate) terminator: Arc<Terminator>,
-    pub(crate) ui_automation: Arc<UiAutomation>
+    pub(crate) form_browser: Arc<FormBrowser>,
+    pub(crate) ui_automation: Arc<UiAutomation>,
 }
 impl Clone for Context {
     fn clone(&self) -> Self {
-        Self{
+        Self {
             commander: self.commander.clone(),
             main_handler: self.main_handler.clone(),
             performer: self.performer.clone(),
             resource_accessor: self.resource_accessor.clone(),
             terminator: self.terminator.clone(),
-            ui_automation: self.ui_automation.clone()
+            form_browser: self.form_browser.clone(),
+            ui_automation: self.ui_automation.clone(),
         }
     }
 }
@@ -52,13 +61,15 @@ impl Context {
      * */
     pub(crate) fn new(terminator: Terminator) -> Self {
         // 创建一个指挥官，用于下发操作命令
-        let commander =Commander::new();
+        let commander = Commander::new();
         // 创建表演者对象（用于把各种信息转换成用户可以感知的形式，例如语音、音效等）
         let performer = Performer::new();
         // 获取一个主线程携程处理器，可以在子线程中调度任务到主线程
         let main_handler = Handle::current();
         // 资源访问器
         let resources = ResourceAccessor::new();
+        // 创建一个窗口浏览
+        let FormBrowser = FormBrowser::new();
         // 创建UiAutomation
         let ui_automation = UiAutomation::new();
         Self {
@@ -67,14 +78,15 @@ impl Context {
             performer: performer.into(),
             resource_accessor: resources.into(),
             terminator: terminator.into(),
-            ui_automation: ui_automation.into()
+            form_browser: FormBrowser.into(),
+            ui_automation: ui_automation.into(),
         }
     }
 
     /**
      * 把上下文对象应用于每一个组件。
      * */
-    pub(crate) fn apply (&self) {
+    pub(crate) fn apply(&self) {
         self.commander.apply(Arc::new(self.clone()))
     }
 
