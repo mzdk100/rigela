717ec24c7c5e9d7f1caf9f4a5cd5f4ad7cf9571c
Administrator <mzdk100@foxmail.com>
2024-01-31 19:59:45 +0800

    修正talent的get_doc()方法返回的值包含Metadata的Path信息； 移除gui macro。

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,18 +1,11 @@
 extern crate proc_macro;
-mod gui;
 mod talent;
 mod utils;
 
-use crate::gui::parse_gui;
 use proc_macro::TokenStream;
 use talent::parse_talent;
 
 #[proc_macro_attribute]
 pub fn talent(args: TokenStream, item: TokenStream) -> TokenStream {
     parse_talent(args.into(), item.into()).into()
-}
-
-#[proc_macro_attribute]
-pub fn gui(args: TokenStream, item: TokenStream) -> TokenStream {
-    parse_gui(args.into(), item.into()).into()
 }
--- macros/src/talent.rs
+++ macros/src/talent.rs
@@ -10,6 +10,7 @@
  * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
  */
+
 use heck::ToUpperCamelCase;
 
 use crate::utils::get_struct_name;
@@ -65,7 +66,7 @@ impl Parse for Metadata {
 pub fn parse_talent(args: TokenStream, item: TokenStream) -> TokenStream {
     let metadata: Metadata = syn::parse2(args).unwrap();
     let doc = metadata.doc;
-    let doc_raw = doc.to_token_stream().to_string();
+    let doc_raw = doc.value.clone();
     let cmd_list = metadata.cmd_list;
     let input: ItemFn = syn::parse2(item).unwrap();
     let id_raw = input.sig.ident.to_string();
