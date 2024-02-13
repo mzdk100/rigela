eb36b31d5557d823421725129d5b54fbaacdc7b6
lwboy <liwenboy2008@126.com>
2024-02-14 00:52:37 +0800

    tts抽取接口

--- main/src/performer/tts/vvtts.rs
+++ main/src/performer/tts/vvtts.rs
@@ -15,7 +15,6 @@ use crate::context::Context;
 use crate::performer::tts::ttsable::{TtsProperty, Ttsable};
 use crate::performer::SAMPLE_RATE;
 use rigela_utils::resample::resample_audio;
-use std::str::FromStr;
 use std::sync::{Arc, OnceLock};
 
 /// VVTTS语音库封装
@@ -28,21 +27,6 @@ impl Default for Vvtts {
         Self {
             context: OnceLock::new(),
         }
-    }
-}
-
-impl Vvtts {
-    #[allow(unused)]
-    pub(crate) fn new() -> Self {
-        Self::default()
-    }
-
-    #[allow(unused)]
-    pub(crate) async fn set_tts_voice_with_vvtts(&self, voice_id: String) {
-        let ctx = self.context.get().unwrap();
-        ctx.proxy32
-            .eci_set_voice(u32::from_str(&voice_id).unwrap())
-            .await
     }
 }
 
@@ -81,12 +65,18 @@ impl Ttsable for Vvtts {
 
     async fn set_value_by_prop(&self, prop: TtsProperty, value: i32) {
         let ctx = self.context.get().unwrap();
+        // let list = ctx.proxy32.eci_get_voices().await;
+
         let mut params = ctx.proxy32.eci_get_voice_params().await;
         match prop {
             TtsProperty::Speed => params.speed = value,
             TtsProperty::Volume => params.volume = value,
             TtsProperty::Pitch => params.pitch_baseline = value,
-            _ => {}
+            TtsProperty::Voice => {
+                // let (_id, _) = list.get(value as usize).unwrap();
+                // ctx.proxy32.eci_set_voice(_id.clone()).await;
+                // return;
+            }
         };
 
         ctx.proxy32.eci_set_voice_params(&params).await;
