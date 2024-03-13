a6607788b19d488ccd9a1063456d95a72561829e
Liwen <Lwboy@Live.com>
2024-03-13 17:11:21 +0800

    字符预处理

--- main/src/performer/text_processing.rs
+++ main/src/performer/text_processing.rs
@@ -14,18 +14,26 @@
 use std::collections::HashMap;
 use std::sync::OnceLock;
 
-pub(crate) fn transform_single_char(char: &char) -> String {
-    if let Some(t) = get_data_map().get(char) {
-        return t.to_string();
+/// 单个字符的预处理
+pub(crate) fn transform_single_char(text: &str) -> String {
+    let mut result = String::new();
+    let mut chars = text.chars();
+    match get_single_char_transform_map().get(&chars.next().unwrap()) {
+        Some(v) => {
+            result.push_str(v);
+            result.push_str(chars.as_str());
+        }
+        None => result.push_str(text),
     }
-    String::from(char.clone())
+
+    result
 }
 
-fn get_data_map() -> HashMap<char, &'static str> {
+fn get_single_char_transform_map() -> HashMap<char, &'static str> {
     OnceLock::new()
         .get_or_init(|| {
             let mut map = HashMap::new();
-            for (k, v) in DATA {
+            for (k, v) in SINGLE_CHAR_TRANSFORM_DATA {
                 map.insert(*k, *v);
             }
             map
@@ -33,4 +41,58 @@ fn get_data_map() -> HashMap<char, &'static str> {
         .to_owned()
 }
 
-const DATA: &[(char, &str)] = &[(' ', "空格"), ('\t', "Tab"), ('\n', "换行"), (',', "逗号")];
+const SINGLE_CHAR_TRANSFORM_DATA: &[(char, &str)] = &[
+    ('!', "叹号"),
+    ('！', "叹号"),
+    ('"', "双引号"),
+    ('“', "左双引号"),
+    ('”', "右双引号"),
+    ('#', "井号"),
+    ('$', "美元"),
+    ('￥', "人民币"),
+    ('%', "百分号"),
+    ('&', "和"),
+    ('\'', "单引号"),
+    ('‘', "左单引号"),
+    ('’', "右单引号"),
+    ('(', "左括号"),
+    ('（', "左括号"),
+    (')', "右括号"),
+    ('）', "右括号"),
+    ('*', "星"),
+    ('+', "加"),
+    (',', "逗号"),
+    ('，', "逗号"),
+    ('-', "减"),
+    ('.', "点"),
+    ('。', "句号"),
+    ('/', "斜杠"),
+    (':', "冒号"),
+    ('：', "冒号"),
+    (';', "分号"),
+    ('；', "分号"),
+    ('<', "小于"),
+    ('《', "左书名号"),
+    ('=', "等于"),
+    ('>', "大于"),
+    ('》', "右书名号"),
+    ('?', "问号"),
+    ('？', "问号"),
+    ('@', "艾特"),
+    ('[', "左方括号"),
+    ('【', "左方括号"),
+    ('\\', "反斜杠"),
+    ('、', "顿号"),
+    (']', "右方括号"),
+    ('】', "右方括号"),
+    ('^', "上尖号"),
+    ('…', "省略号"),
+    ('_', "下划线"),
+    ('—', "破折号"),
+    ('`', "反撇号"),
+    ('·', "圆点"),
+    ('{', "左花括号"),
+    ('|', "竖杠"),
+    ('}', "右花括号"),
+    ('~', "波浪号"),
+];
