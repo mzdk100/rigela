0247ffa7169b51923683d40a56644519067773fd
Administrator <mzdk100@foxmail.com>
2024-03-26 08:10:32 +0800

    增加一个条件属性，在调试模式下显示CMD窗口（方便使用shell调试），发布模式禁用此行为。
    Signed-off-by: Administrator <mzdk100@foxmail.com>

--- main/src/main.rs
+++ main/src/main.rs
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-#![windows_subsystem = "windows"]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 #[macro_use]
 extern crate rust_i18n;
